1065507883dbc5f873aedf587435bcddfc9c5589
takumi_Mac <>
2024-04-18 01:47:23 +0900

    first commit

---
+++ src/hooks/useGlobalState.ts
@@ -0,0 +1,42 @@
+import useSWR from 'swr'
+
+export const useUserState = () => {
+  type userStateType = {
+    id: number
+    name: string
+    email: string
+    isSignedIn: boolean
+    isFetched: boolean
+  }
+  const fallbackData: userStateType = {
+    id: 0,
+    name: '',
+    email: '',
+    isSignedIn: false,
+    isFetched: false,
+  }
+  const { data: state, mutate: setState } = useSWR('user', null, {
+    fallbackData: fallbackData,
+  })
+  return [state, setState] as [userStateType, (value: userStateType) => void]
+}
+export const useSnackbarState = () => {
+  type snackbarStateType = {
+    message: null | string
+    severity: null | 'success' | 'error'
+    pathname: null | string
+  }
+  //fallbackDataはデフォルトを設定する
+  const fallbackData: snackbarStateType = {
+    message: null,
+    severity: null,
+    pathname: null,
+  }
+  const { data: state, mutate: setState } = useSWR('snackbar', null, {
+    fallbackData: fallbackData,
+  })
+  return [state, setState] as [
+    snackbarStateType,
+    (value: snackbarStateType) => void,
+  ]
+}
