7feae348aeb1626535dc9ba8f2d7b61f5d322051
takumi_Mac <>
2024-05-14 01:57:13 +0900

    indexのconsole.logを消す

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,6 @@ const Index: NextPage = () => {
   const router = useRouter()
   const page = 'page' in router.query ? Number(router.query.page) : 1
   const url = process.env.NEXT_PUBLIC_API_BASE_URL + '/articles/?page=' + page
-  console.log(url)
   const { data, error } = useSWR(url, fetcher)
   if (error) return <Error />
   if (!data) return <Loading />
