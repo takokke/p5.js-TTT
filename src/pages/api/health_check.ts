f9f2ebdc65fb59733c447979abda0d10e75052f2
takumi_Mac <>
2024-05-29 18:09:35 +0900

    RailsからmicroCMSに変更

--- src/pages/api/health_check.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200)
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ status: 'ok' }))
-}
